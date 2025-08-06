94c9e096bb44010bd5bba3af5af9006297982a5e
Stasik22 <111745713+Stasik22@users.noreply.github.com>
2025-08-06 14:53:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from Buttons import site_buttons
 from Buttons import app_buttons
 
 
-API_TOKEN = '7417043537:AAHzByL8I3DAnnOFqQsqQOfDQXa7-lD8eLI'
+API_TOKEN = '7417043537:AAF7jkxbF4pgujPaTq8E34FP2HCrErdLy60'
 bot = tb.TeleBot(API_TOKEN)
 
 url = "https://www.reddit.com"
